9f9de5852aaaf5dfdf3fcd70089f3bddaf6fca91
Jean steven rojas tumayquispe <1913110180@untels.edu.pe>
2023-06-12 14:18:59 -0500

    cuarto commit

--- DAMSem10_RecyclerView/app/src/main/java/com/example/damsem10_recyclerview/MainActivity.java
+++ DAMSem10_RecyclerView/app/src/main/java/com/example/damsem10_recyclerview/MainActivity.java
@@ -3,30 +3,12 @@ package com.example.damsem10_recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnToast;
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnToast = findViewById(R.id.btnfinalizar);
-        btnToast.setOnClickListener(this);
-
-    }
-    @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.btnfinalizar){
-            Toast.makeText(this,"MATRICULADO",Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
     }
 }
